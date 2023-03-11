4471a7c64d69cc4807c8a08bff67d46a475a314e
Kemar Norton <kemarnorton@hotmail.com>
2023-03-10 21:53:10 -0500

    Add Index page for customer

---
+++ ResturantWebApp/Pages/Customer/Home/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ResturantWebApp.DataAccess.Repository.IRepository;
+using ResturantWebApp.Models;
+
+namespace ResturantWebApp.Pages.Customer.Home
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<MenuItem> MenuItemList { get; set; }
+		public IEnumerable<Category> CategoryList { get; set; }
+
+		public void OnGet()
+        {
+            MenuItemList = _unitOfWork.MenuItem.GetAll(includeProperties: "Category,FoodType");
+
+            CategoryList = _unitOfWork.Category.GetAll();
+
+        }
+    }
+}
--- ResturantWebApp/wwwroot/js/menuItem.js
+++ ResturantWebApp/wwwroot/js/menuItem.js
@@ -41,8 +41,8 @@ Delete = function (url) {
         text: "You won't be able to revert this!",
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#228B22',
         confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
         if (result.isConfirmed) {
